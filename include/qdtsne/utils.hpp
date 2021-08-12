ef9c6a45bd3d524f613a3da1e342df4126abde21
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-08-11 22:35:20 -0700

    Use a Box-Muller transform instead of random's normal_dist.

--- include/qdtsne/utils.hpp
+++ include/qdtsne/utils.hpp
@@ -8,11 +8,25 @@
  */
 
 #include <random>
+#include <cmath>
 
 namespace qdtsne {
 
+template<class Engine>
+double uniform01 (Engine& eng) {
+    // Stolen from Boost.
+    const double factor = 1.0 / static_cast<double>((eng.max)()-(eng.min)());
+    double result;
+    do {
+        result = static_cast<double>(eng() - (eng.min)()) * factor;
+    } while (result == 1.0);
+    return result;
+}
+
 /**
- * Initializes the starting locations of each observation in the embedding. 
+ * Initializes the starting locations of each observation in the embedding.
+ * We do so using our own implementation of the Box-Muller transform,
+ * to avoid problems with differences in the distribution functions across C++ standard library implementations.
  *
  * @tparam ndim Number of dimensions.
  *
@@ -25,10 +39,30 @@ namespace qdtsne {
 template<int ndim = 2>
 void initialize_random(double* Y, size_t N, int seed = 42) {
     std::mt19937_64 rng(seed);
-    std::normal_distribution<> dist(0, 1);
-    for (size_t i = 0; i < N * ndim; ++i) {
-        Y[i] = dist(rng);
+    constexpr double pi = 3.14159265358979323846;
+
+    size_t total = N * ndim;
+    bool odd = total % 2;
+    if (odd) {
+        --total;
     }
+
+    // Box-Muller gives us two random values at a time.
+    for (size_t i = 0; i < total; i += 2) {
+        double constant = std::sqrt(-2 * std::log(uniform01(rng)));
+        double angle = 2 * pi * uniform01(rng);
+        Y[i] = constant * std::sin(angle);
+        Y[i + 1] = constant * std::cos(angle);
+    }
+
+    if (odd) {
+        // Adding the poor extra for odd total lengths.
+        double constant = std::sqrt(-2 * std::log(uniform01(rng)));
+        double angle = 2 * pi * uniform01(rng);
+        Y[total] = constant * std::sin(angle);
+    }
+
+    return;
 }
 
 /**
---
+++ tests/src/utils.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include "qdtsne/utils.hpp"
+
+class InitTester : public ::testing::TestWithParam<std::tuple<int, int> > {};
+
+TEST_P(InitTester, Normality) {
+    auto param = GetParam();
+    auto Y = qdtsne::initialize_random<>(std::get<0>(param), std::get<1>(param));
+
+    EXPECT_NE(Y.back(), 0); // last element should be filled correctly in the odd case.
+
+    std::sort(Y.begin(), Y.end());
+
+    EXPECT_TRUE(Y.front() < 0);
+    EXPECT_TRUE(Y.back() > 0);
+
+    double mean = std::accumulate(Y.begin(), Y.end(), 0.0) / Y.size();
+    EXPECT_TRUE(mean > -0.1);
+    EXPECT_TRUE(mean < 0.1);
+
+    double var = 0;
+    for (auto y : Y) {
+        var += (y - mean) * (y - mean);
+    }
+    var /= Y.size() - 1;
+    EXPECT_TRUE(var > 0.9);
+    EXPECT_TRUE(var < 1.1);
+
+    for (size_t i = 1; i < Y.size(); ++i) {
+        EXPECT_TRUE(Y[i-1] < Y[i]);
+    }
+}
+
+INSTANTIATE_TEST_CASE_P(
+    InitTests,
+    InitTester,
+    ::testing::Combine(
+        ::testing::Values(1000, 1001), // even AND odd
+        ::testing::Values(42, 100, 0) // various seeds
+    )
+);

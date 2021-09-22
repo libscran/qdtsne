954405877f8fe976f6665448ea27a741fbfa717c
LTLA <LTLA@users.noreply.github.com>
2021-09-22 05:44:24 +0000

    Deploying to gh-pages from @ LTLA/qdtsne@83e544480d8a62d8807bba1da0a3526e0bc0b3dd 🚀

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -3,5 +3,6 @@ var searchData=
   ['infer_5fperplexity_0',['infer_perplexity',['../structqdtsne_1_1Tsne_1_1Defaults.html#a829bfb892c4599423ab37646906a7a30',1,'qdtsne::Tsne::Defaults']]],
   ['initialize_1',['initialize',['../classqdtsne_1_1Tsne.html#aa56a41100785c53e9d049a52ce83f352',1,'qdtsne::Tsne::initialize(NeighborList&lt; Index &gt; nn)'],['../classqdtsne_1_1Tsne.html#a38e99fd21499b2bc6079a723b60dd982',1,'qdtsne::Tsne::initialize(const Input *input, size_t D, size_t N)'],['../classqdtsne_1_1Tsne.html#a7900a41575c8388d5ac055283dff9a50',1,'qdtsne::Tsne::initialize(const Algorithm *searcher)']]],
   ['initialize_5frandom_2',['initialize_random',['../utils_8hpp.html#adcce1a11ba75b593bd7b0468cee39f72',1,'qdtsne::initialize_random(double *Y, size_t N, int seed=42)'],['../utils_8hpp.html#a97c2692b4fdb0a120f81bf7b6e06891b',1,'qdtsne::initialize_random(size_t N, int seed=42)']]],
-  ['iteration_3',['iteration',['../structqdtsne_1_1Tsne_1_1Status.html#ad83e9bcd93aa7aa7114dc429444c97ee',1,'qdtsne::Tsne::Status']]]
+  ['interpolation_3',['interpolation',['../structqdtsne_1_1Tsne_1_1Defaults.html#ab65c4af2f5d2dc300faccb4df49a419e',1,'qdtsne::Tsne::Defaults']]],
+  ['iteration_4',['iteration',['../structqdtsne_1_1Tsne_1_1Status.html#ad83e9bcd93aa7aa7114dc429444c97ee',1,'qdtsne::Tsne::Status']]]
 ];
--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['infer_5fperplexity_0',['infer_perplexity',['../structqdtsne_1_1Tsne_1_1Defaults.html#a829bfb892c4599423ab37646906a7a30',1,'qdtsne::Tsne::Defaults']]]
+  ['infer_5fperplexity_0',['infer_perplexity',['../structqdtsne_1_1Tsne_1_1Defaults.html#a829bfb892c4599423ab37646906a7a30',1,'qdtsne::Tsne::Defaults']]],
+  ['interpolation_1',['interpolation',['../structqdtsne_1_1Tsne_1_1Defaults.html#ab65c4af2f5d2dc300faccb4df49a419e',1,'qdtsne::Tsne::Defaults']]]
 ];

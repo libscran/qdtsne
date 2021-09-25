b6af849eb2fab129ed45832366050e773a90a920
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-25 00:14:51 -0700

    Minor fixes to the umbrella header.

--- include/qdtsne/qdtsne.hpp
+++ include/qdtsne/qdtsne.hpp
@@ -2,13 +2,16 @@
 #define QDTSNE_QDTSNE_HPP
 
 /**
- * @file
+ * @file qdtsne.hpp
  *
  * @brief Umbrella header for all **qdtsne** files.
+ *
+ * This header is effectively an alias for `tsne.hpp`.
+ * It is provided just to allow developers to `#include "qdtsne/qdtsne.hpp"` without thinking too hard about the code-containing headers.
+ * Of course, `#include qdtsne/tsne.hpp` also works but is less obvious.
  */
 
 #include "tsne.hpp"
-#include "sptree.hpp"
 #include "utils.hpp"
 
 #endif

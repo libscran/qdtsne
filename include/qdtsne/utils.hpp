7b0b282ccb680dab66bdb0c779862ee76a58cf2d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-27 13:04:38 +1100

    Updated parallelize() override to return number of used workers.
    This is consistent with the updated subpar::parallelize_range() function. Also
    fleshed out the docs to better describe why and how to override it.
    
    Also updated the versions of dependencies and bumped version for new release.

--- include/qdtsne/utils.hpp
+++ include/qdtsne/utils.hpp
@@ -117,21 +117,28 @@ std::vector<Float_> initialize_random(const std::size_t num_points, const typena
  * @tparam Run_ Function to execute a range of tasks.
  *
  * @param num_workers Number of workers.
+ * This should be positive.
  * @param num_tasks Number of tasks.
- * @param run_task_range Function to iterate over a range of tasks within a worker.
+ * This should be non-negative.
+ * @param run_task_range Function to iterate over a range of tasks within a worker,
+ * see the argument of the same name in `subpar::parallelize_range()`.
+ *
+ * return Number of used workers.
+ * This will be no greater than `num_workers`.
  *
  * By default, this is an alias to `subpar::parallelize_range()`.
- * However, if the `QDTSNE_CUSTOM_PARALLEL` function-like macro is defined, it is called instead. 
- * Any user-defined macro should accept the same arguments as `subpar::parallelize_range()`.
+ * Its purpose is to enable **qdtsne**-specific customization to the parallelization scheme without affecting other libraries that use **subpar**.
+ * If the `QDTSNE_CUSTOM_PARALLEL` macro is defined, it will be used instead of `subpar::parallelize_range()` whenever `parallelize()` is called. 
+ * Any user-defined macro should follow the same requirements as the `SUBPAR_CUSTOM_PARALLELIZE_RANGE` override for `subpar::parallelize_range()`.
  */
 template<typename Task_, class Run_>
-void parallelize(const int num_workers, const Task_ num_tasks, Run_ run_task_range) {
+int parallelize(const int num_workers, const Task_ num_tasks, Run_ run_task_range) {
 #ifndef QDTSNE_CUSTOM_PARALLEL
     // Don't make this nothrow_ = true, there's too many allocations and the
     // derived methods for the nearest neighbors search could do anything...
-    subpar::parallelize(num_workers, num_tasks, std::move(run_task_range));
+    return subpar::parallelize_range(num_workers, num_tasks, std::move(run_task_range));
 #else
-    QDTSNE_CUSTOM_PARALLEL(num_workers, num_tasks, run_task_range);
+    return QDTSNE_CUSTOM_PARALLEL(num_workers, num_tasks, run_task_range);
 #endif
 }
 
